165adb4eb1fd82916217f66cd96b5b3d04dc4c89
Igor Kovorotniy <i.kovorotniy@2050.digital>
2020-07-06 13:51:40 +0300

    Refactoring, testend on 2 sheets in 1 file

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
 from openpyxl import load_workbook, workbook, worksheet
 from datetime import datetime
 import json
-
+import functions
 wb = load_workbook('sh1.xlsx')
 
 inter_dict = {}
 target_dict = {}
 headers = []
 values = []
-row_count = wb['Sheet1'].max_row
-col_count = wb['Sheet1'].max_column
+# row_count = wb['Sheet1'].max_row
+# col_count = wb['Sheet1'].max_column
 counter = 0
 
-for row in wb['Sheet1'].iter_rows(min_row=0, max_col=col_count, max_row=row_count):
-  for i, cell in enumerate(row):
-    counter += 1
-    if (len(headers) == col_count):
-      # try to parse date as string to format
-      if ('DATE' in headers[i] or 'TIME' in headers[i]):
-        if (not cell.value):
-          date = ''
-          inter_dict.update({ headers[i]:  date })
-        # cell value not none
-        if (cell.value):
-          date = datetime.strftime(cell.value, '%Y-%m-%dT%H:%M:%SZ')
-          inter_dict.update({ headers[i]:  date })
-      else:
-        inter_dict.update({ headers[i]: cell.value })
+sheets = functions.get_excel_sheets(wb)
 
-    if (len(headers) != col_count):
-      headers.append(cell.value)
-    
-    
-    if (counter == col_count):
-      counter = 0
-      if (inter_dict):
-        values.append(inter_dict)
-        inter_dict = {}
+for current_sheet in sheets:
+  row_count = wb[current_sheet].max_row
+  col_count = wb[current_sheet].max_column
 
-if (values):
-  target_dict.update({ 'Sheet1': values })
-  print(target_dict)
-  with open('output.json', 'w') as output:
+  print(row_count)
+  print(col_count)
+
+  print(current_sheet)
+
+  for row in wb[current_sheet].iter_rows(min_row=0, max_col=col_count, max_row=row_count):
+    for i, cell in enumerate(row):
+      counter += 1
+      if (len(headers) == col_count):
+        # try to parse date as string to format
+        if ('DATE' in headers[i] or 'TIME' in headers[i]):
+          if (not cell.value):
+            date = ''
+            inter_dict.update({ headers[i]:  date })
+          # cell value not none
+          if (cell.value):
+            date = datetime.strftime(cell.value, '%Y-%m-%dT%H:%M:%SZ')
+            inter_dict.update({ headers[i]:  date })
+        else:
+          inter_dict.update({ headers[i]: cell.value })
+      if (len(headers) != col_count):
+        headers.append(cell.value)
+      if (counter == col_count):
+        counter = 0
+        if (inter_dict):
+          values.append(inter_dict)
+          inter_dict = {}
+  
+  if (values):
+    target_dict.update({ current_sheet: values })
+    # print(target_dict)
+    values = []
+    headers = []
+    inter_dict = {}
+    counter = 0
+
+with open('output.json', 'w') as output:
     json.dump(target_dict, output)
