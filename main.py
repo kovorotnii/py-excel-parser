2d1328993d0e904d7a510f3763ab3d7c005cf730
Igor Kovorotniy <i.kovorotniy@2050.digital>
2020-07-07 14:24:54 +0300

    Refactoring

--- main.py
+++ main.py
@@ -16,19 +16,24 @@ for current_sheet in sheets:
   row_count = wb[current_sheet].max_row
   col_count = wb[current_sheet].max_column
 
-  print(row_count)
-  print(col_count)
-  print(current_sheet)
+  print('Parsing sheet: ', current_sheet)
 
-  if current_sheet == 'loco_26':
-
-    values = functions.parse_excel(wb, current_sheet, col_count, row_count)
+  values = functions.parse_excel(wb, current_sheet, col_count, row_count)
   
-    if (values):
-      target_dict.update({ current_sheet: values })
-      values = []
-
-with open('output.json', 'w') as output:
+  if not values:
+    print("Nothing to assign to target object!")
+    quit()
+  # Get data from parser, assign to dict
+  target_dict.update({ current_sheet: values })
+  values = []
+    
+try:
+  with open('output.json', 'w') as output:
     data = json.dumps(target_dict, ensure_ascii=False)
     output.write(data)
     output.close()
+  
+  print('Parsing completed!')
+except IOError as e:
+  print('Writing to file error! ', e)
+  quit()
